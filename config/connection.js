e38773f1cd1b64e0e3696146a8f6af2fc5fa3f91
Pedal <jmb181@humboldt.edu>
2020-12-13 23:56:20 -0800

    project setup

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// import the Squelize constructor from the library
+const Sequelize = require('sequelize');
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
