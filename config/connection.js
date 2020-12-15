a291b068aa4957917de15f547397d56d1f598c70
Pedal <jmb181@humboldt.edu>
2020-12-14 23:29:58 -0800

    route setup and a lot ofiles added

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 // import the Squelize constructor from the library
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
