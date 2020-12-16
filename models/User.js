28a272f23578c791036bc069aea6d0cfa75b7fed
Pedal <jmb181@humboldt.edu>
2020-12-15 23:52:17 -0800

    user email validation

--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model - inherits from Model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,6 +51,29 @@ router.post('/', (req,res) => {
     });
 });
 
+// POST api/users/login login route
+router.post('/login', (req, res) => {
+    // expects {email: 'email@hotmail.com', password: 'hunter2'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }) .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' })
+    })
+});
+
 // PUT /api/users/1 Update User 
 router.put('/:id', (req,res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
