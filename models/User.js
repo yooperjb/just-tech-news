cc7eaa68123e122f3a255ebb76bc49795fccb3ed
Pedal <jmb181@humboldt.edu>
2020-12-15 21:49:04 -0800

    hashed password for put

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 // import Model Class and DataTypes object
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model - inherits from Model
 class User extends Model {}
@@ -48,6 +49,19 @@ User.init(
     }
   },
   {
+    // hooks added to the second object in init
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password,10);
+            return newUserData;
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password,10);
+            return updatedUserData;
+        }
+    },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,11 +51,12 @@ router.post('/', (req,res) => {
     });
 });
 
-// PUT /api/users/1
+// PUT /api/users/1 Update User 
 router.put('/:id', (req,res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
      // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
      User.update(req.body, {
+         individualHooks: true,
          where: {
              id: req.params.id
          }
