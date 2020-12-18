dbc679ec6e8c9433c10f9941d8f4cea6bc159f43
Pedal <jmb181@humboldt.edu>
2020-12-18 14:22:03 -0800

    fixed process typo

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// import the Squelize constructor from the library
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
@@ -6,7 +6,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(processs.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
@@ -14,9 +14,6 @@ if (process.env.JAWSDB_URL) {
         port: 3306
     });
 }
-
-
-
 
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 //     host: 'localhost',
