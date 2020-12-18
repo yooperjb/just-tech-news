c737f0ec80614411f104fdf19fda0720e33178ae
Pedal <jmb181@humboldt.edu>
2020-12-17 23:38:30 -0800

    I have no idea what just happened

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const { truncate } = require('./User');
 
-// crete our Post model
-class Post extends Model {}
+// create our Post model
+class Post extends Model {
+    static upvote(body, models){
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
 
 // create fields/columns for Post Model
 Post.init(
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 const { route } = require('./user-routes');
+const sequelize = require('../../config/connection');
 
 // GET all Posts /api/posts
 router.get('/', (req, res) => {
     console.log('+++++++++++++++++');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -28,7 +29,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -49,7 +56,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// POST Post /api/post
+// POST Post /api/posts
 router.post('/', (req, res) => {
     // expects {title:, post_url:, user_id:}
     Post.create({
@@ -62,6 +69,17 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+// PUT /api/posts/upvote - update vote count
+router.put('/upvote', (req, res) => {
+// custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 // PUT route to update Post title /api/post/:id
