c59291fcd7a66b1e73af90d7c636a1f20d1b0f81
Pedal <jmb181@humboldt.edu>
2020-12-30 13:18:45 -0800

    Partials and helper functions

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
     secret: 'Super secret secret',
@@ -32,8 +34,6 @@ app.use(session(sess));
 
 // turn on routes
 app.use(routes);
-
-
 
 // turn on connection to db and server
 // force: false stops from dropping tables each time
