dd376eabfba1c7e2daa54751344e7c7ba95cac98
Pedal <jmb181@humboldt.edu>
2022-11-19 17:25:39 -0800

    code cleanup

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -12,6 +12,7 @@ test('format_plural() correctly pluralizes words', () => {
     expect(format_plural("Lion", 1)).toBe('Lion');
 });
 
+// test format url
 test('format_url() returns a simplified URL string', () => {
     const url1 = format_url('http://test.com/page/1');
     const url2 = format_url('https://www.coolstuff.com/abcdefg/');
